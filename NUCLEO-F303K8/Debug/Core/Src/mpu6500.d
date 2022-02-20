75a57d0d0b01ca5ac7e7131b58fd91da39f2779a
Teruru-52 <83484632+Teruru-52@users.noreply.github.com>
2022-02-21 03:11:02 +0900

    if文の位置を修正してprintf文で出力数を合わせた

--- NUCLEO-F303K8/Debug/Core/Src/mpu6500.d
+++ NUCLEO-F303K8/Debug/Core/Src/mpu6500.d
@@ -18,8 +18,6 @@ Core/Src/mpu6500.o: ../Core/Src/mpu6500.cpp ../Core/Inc/mpu6500.hpp \
  ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
  ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
  ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
- ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc.h \
- ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc_ex.h \
  ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
  ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
  ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
@@ -74,10 +72,6 @@ Core/Src/mpu6500.o: ../Core/Src/mpu6500.cpp ../Core/Inc/mpu6500.hpp \
 ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h:
 
 ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
-
-../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc.h:
-
-../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc_ex.h:
 
 ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
 
